8195081a6f89da85dba5357243b6681d3df12f30
Luu Thuy Minh Nhat <minhnhat240820032019@gmail.com>
2023-08-30 15:58:44 +0700

    add project TamGiac

---
+++ TamGiac/TamGiac/HinhTamGiac.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TamGiac
+{
+    class HinhTamGiac
+    {
+        private double ma, mb, mc;
+
+        public HinhTamGiac()
+        {
+            ma = mb = mc = 0;
+        }
+
+        public HinhTamGiac(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0 || (a + b <= c) || (a + c <= b) || (b + c <= a))
+            {
+                ma = mb = mc = 0;
+            }
+            else
+            {
+                ma = a;
+                mb = b;
+                mc = c;
+            }
+        }
+
+        public double Ma
+        {
+            get { return ma; }
+            set
+            {
+                if (value > 0 && value + mb > mc && value + mc > mb)
+                    ma = value;
+            }
+        }
+
+        public double Mb
+        {
+            get { return mb; }
+            set
+            {
+                if (value > 0 && value + ma > mc && value + mc > ma)
+                    mb = value;
+            }
+        }
+
+        public double Mc
+        {
+            get { return mc; }
+            set
+            {
+                if (value > 0 && value + ma > mb && value + mb > ma)
+                    mc = value;
+            }
+        }
+
+        public double TinhChuVi()
+        {
+            return ma + mb + mc;
+        }
+
+        public double TinhDienTich()
+        {
+            double p = TinhChuVi() / 2;
+            return Math.Sqrt(p * (p - ma) * (p - mb) * (p - mc));
+        }
+
+        public string KieuTamGiac()
+        {
+            if (ma == mb && mb == mc)
+                return "Tam giac đeu";
+            else if (ma == mb || mb == mc || ma == mc)
+                return "Tam giac can";
+            else if (ma != mb && mb != mc && ma != mc)
+                return "Tam giac thuong";
+            else
+                return "Khong phai tam giac";
+        }
+        public void xuatTT()
+        {
+            Console.WriteLine("{0} \t {1} \t {2} \t {3} \t {4} \t {5}", ma, mb, mc, KieuTamGiac(), TinhChuVi(), TinhDienTich());
+        }
+    }
+}
---
+++ TamGiac/TamGiac/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TamGiac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HinhTamGiac HTG1 = new HinhTamGiac(3, 5, 6); // Tam giác thường
+            HinhTamGiac HTG2 = new HinhTamGiac(-1, 2, 3); // Hình vi phạm ràng buộc
+            HinhTamGiac HTG3 = new HinhTamGiac(8, 8, 8); // Tam giác đều
+            HinhTamGiac HTG4 = new HinhTamGiac(6, 4, 6); // Tam giác cân
+            HinhTamGiac HTG5 = new HinhTamGiac(11, 10, 13); // Tam giác thường
+            HTG1.xuatTT();
+            Console.ReadLine();
+        }
+    }
+}
