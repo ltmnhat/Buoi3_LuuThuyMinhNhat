a3483f153f86e399ad71d52840c07b3ec38b1800
Luu Thuy Minh Nhat <minhnhat240820032019@gmail.com>
2023-08-30 14:34:35 +0700

    add new file ToaDo

---
+++ ToaDo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToaDo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToaDoXY td = new ToaDoXY();
+            td.xuat();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ToaDo/ToaDoXY.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToaDo
+{
+    class ToaDoXY
+    {
+        int x;
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        int y;
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        string tentoado;
+
+        public string Tentoado
+        {
+            get { return tentoado; }
+            set { tentoado = value; }
+        }
+        public ToaDoXY()
+        {
+            x = 0;
+            y = 0;
+            tentoado = "A";
+        }
+        public ToaDoXY(int x,int y,string tentoado)
+        {
+            x = this.x;
+            y = this.y;
+            tentoado = this.tentoado;
+        }
+        public void xuat()
+        {
+            Console.WriteLine("Tra ve thong tin theo mau");
+            Console.WriteLine("{0}({1}:{2})", tentoado, x, y);
+        }
+    }
+}
