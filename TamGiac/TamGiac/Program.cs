2367b9160c0c53d337046ad3fcbbae8d3f9b9022
Luu Thuy Minh Nhat <minhnhat240820032019@gmail.com>
2023-08-30 16:02:31 +0700

    add demo

--- TamGiac/TamGiac/Program.cs
+++ TamGiac/TamGiac/Program.cs
@@ -15,7 +15,7 @@ namespace TamGiac
             HinhTamGiac HTG3 = new HinhTamGiac(8, 8, 8); // Tam giác đều
             HinhTamGiac HTG4 = new HinhTamGiac(6, 4, 6); // Tam giác cân
             HinhTamGiac HTG5 = new HinhTamGiac(11, 10, 13); // Tam giác thường
-            HTG1.xuatTT();
+            HTG3.xuatTT();
             Console.ReadLine();
         }
     }
